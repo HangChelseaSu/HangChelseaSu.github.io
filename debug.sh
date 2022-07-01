248534a68048b01842d10c976def337ed8b02ff7
HangChelseaSu <hangsu@mit.edu>
2022-07-01 16:12:33 -0400

    a

--- debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl \
-  -X POST \
-  -H "Accept: Applications/vnd.github+json" \
-  -H "Authorization: token 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" \
-https://api.github.com/repos/HangChelseaSu/HangChelseaSu.github.io/pages/builds
